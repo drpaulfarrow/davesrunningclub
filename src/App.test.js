26f858ce284710047f4b2bc6210c41cc72c4f48e
Paul Farrow <paulandrewfarrow@gmail.com.com>
2025-08-04 11:30:24 +0100

    Hopefull fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test("renders Dave's Running Club title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Dave's Running Club/i);
+  expect(titleElement).toBeInTheDocument();
 });
