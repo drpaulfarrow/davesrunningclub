387f231c57fb39bad73a508f2d17cbd08dd53fbf
Paul Farrow <paulandrewfarrow@gmail.com.com>
2025-08-04 11:41:52 +0100

    Again, trying to fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test("renders Dave's Running Club title", () => {
+test("renders Dave's Running Club title somewhere on the page", () => {
   render(<App />);
-  const titleElement = screen.getByText(/Dave's Running Club/i);
-  expect(titleElement).toBeInTheDocument();
+  const matches = screen.getAllByText(/Dave's Running Club/i);
+  expect(matches.length).toBeGreaterThan(0);
 });
